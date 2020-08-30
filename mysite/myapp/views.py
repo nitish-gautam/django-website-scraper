125490831068a9214cef0a3b48f07e0e4bc58dfe
Nitish Gautam <nitishgautam.uob.com>
2020-08-30 23:07:14 +0100

    initial functionality added

---
+++ mysite/myapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect
+from .models import Link
+# Create your views here.
+
+'''
+def scrape(request):
+    page = requests.get('https://www.facebook.com')
+    soup = BeautifulSoup(page.text,'html.parser')
+    
+    link_address = []
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+        
+    return render(request, 'myapp/result.html', {'link_address': link_address})
+        
+
+'''
+def scrape(request):
+    if request.method == "POST":
+        site = request.POST.get('site', '')
+
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.string
+            Link.objects.create(address=link_address, name=link_text)
+        return HttpResponseRedirect('/')
+    else:
+        data = Link.objects.all()
+
+    return render(request, 'myapp/result.html', {'data': data})
+
+
+def clear(request):
+    Link.objects.all().delete()
+    return render(request, 'myapp/result.html')
