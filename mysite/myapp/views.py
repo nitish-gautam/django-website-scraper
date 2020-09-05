56a836c75064b17d6dc3502a2998b7c318f9ffb2
Nitish Gautam <nitishgautam.uob.com>
2020-09-05 06:30:44 +0100

    updated working copy

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,21 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from django.http import HttpResponseRedirect
 from .models import Link
+
 # Create your views here.
-
-'''
-def scrape(request):
-    page = requests.get('https://www.facebook.com')
-    soup = BeautifulSoup(page.text,'html.parser')
-    
-    link_address = []
-    for link in soup.find_all('a'):
-        link_address.append(link.get('href'))
-        
-    return render(request, 'myapp/result.html', {'link_address': link_address})
-        
-
-'''
 def scrape(request):
     if request.method == "POST":
         site = request.POST.get('site', '')
